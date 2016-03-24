058c9dd086744137555349fa82c2ab1607824307
Thibaut Broggi <broggi_t@epitech.eu>
2016-03-24 12:37:38 +0100

    Add C++11 random initalization

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
+#include <random>
 #include "World.hpp"
 #include "systems/console.cpp"
 
 int main(void) {
   Ecs::World w;
+  std::random_device rd;
+  std::srand(rd());
 
   w.addSystem<System::Console>();
   w.run();
