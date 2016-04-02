9f9a4cab06250e2ba67d48f69e2a6280cf7d664c
Thibaut Broggi <broggi_t@epitech.eu>
2016-04-02 03:55:49 +0200

    Add a global structure with infos about resources

--- src/Resources.hpp
+++ src/Resources.hpp
@@ -1,7 +1,22 @@
 #pragma once
 
+#include <cstdint>
+
 enum Resource {
   TIMBER, BRICK,
   FIRST = TIMBER, LAST = BRICK,
   LENGTH = LAST + 1
+};
+
+static const struct {
+  uint8_t size;
+  struct {
+    uint16_t base;
+    uint16_t poor;
+    uint16_t wealthy;
+    uint16_t rich;
+  } consumption;
+} infosResource[] = {
+  {10, {5, 1, 0, 0}},
+  {10, {7, 1, 0, 0}}
 };
